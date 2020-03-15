a2bd1fa1770666f5b977a1983d5550f967ab75fe
esoft11 - Daniel Barreto <dandpb@gmail.com>
2020-03-15 11:37:27 -0300

    add environment varieble

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'api_nova_escola_db',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamp: true,
     underscored: true,
